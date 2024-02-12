25c54b1b4769db886a1f888160a19d2189c534f1
srinu orsu <132943635+developersrinu@users.noreply.github.com>
2024-02-12 17:55:12 +0530

    Update index.js
    ok

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './redux-store/store';
 import axios from 'axios';
-axios.defaults.baseURL = 'https://filmbackend-gu8dsoegg-developersrinu.vercel.app/'
+axios.defaults.baseURL = 'https://filmbackend.vercel.app/'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
